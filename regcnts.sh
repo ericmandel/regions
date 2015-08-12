6c84f350065c4f2bbaba618e985c53cf07110ecd
Eric Mandel <eric@cfa.harvard.edu>
2015-08-12 14:36:54 -0400

    add regcnts.sh script:   -- used in ds9 processing   -- will calculate a block factor to pass to regcnts for binary tables   -- needed for X-ray tables whose image dimensions are huge (e.g. XMM data)

--- regcnts.sh
+++ regcnts.sh
@@ -1,54 +1,73 @@
 #!/bin/bash
+# set -x
 
-if [ $# -lt 1 ]; then
-  echo $0 ifile1 ...
-  exit 1
-fi
-
-# max size image we will generate from a table
-MAXDIM=8192
-# starting block factor
-BL=8
-
-hash fkeyprint 1>/dev/null 2>&1
-if [ $? = 0 ]; then
-  XEQ=ftools
-else
-  hash funimage 1>/dev/null 2>&1
-  if [ $? = 0 ]; then
-    XEQ=funtools
-  else
-    error "requires either ftools (fkeyprint) or funtools (fumimage)"
-  fi
-fi
-
-while [ x"$1" != x ]; do
-  # new file
-  IFILE="$1"
-  shift
-
-  # get image dimensions
-  case $XEQ in
-      ftools)
-	NAXIS1=`fkeyprint infile=$IFILE'[events][bin '$BL']' keynam=NAXIS1 | egrep = | awk -F= '{print $2}' | awk -F/ '{print $1}' | sed 's/^  *//g;s/  *$//g'`
-	NAXIS2=`fkeyprint infile=$IFILE'[events][bin '$BL']' keynam=NAXIS2 | egrep = | awk -F= '{print $2}' | awk -F/ '{print $1}' | sed 's/^  *//g;s/  *$//g'`
-	;;
-      funtools)
-	DIMS=`funimage $IFILE'[*,*,'$BL']' stdout bitpix=8 2>/dev/null | dd bs=80 count=10 2>/dev/null | sed -e 's/.\{80\}/&@/g' | tr '@' '\012' | egrep NAXIS'[1,2]' | sed 's# /.*##g;s# ##g'`
-	eval $DIMS
-	;;
-  esac
-
-  # get a reasonable block factor to make the image dimensions "finite"
-  BL1=`echo "$NAXIS1 * $BL / $MAXDIM" | bc`
-  BL2=`echo "$NAXIS2 * $BL / $MAXDIM" | bc`
-  if [ $BL1 -gt $BL2 ]; then
-    BL="$BL1"
-  else
-    BL="$BL2"
-  fi
-
-  # run regcnts using this block factor
-  ./regcnts -b $BL $IFILE $*
+# look for required input file argument
+for var in "$@"; do
+    case $var in
+	-*) continue;;
+         *) IFILE="$var"; break;;
+    esac
 done
+if [ x"$IFILE" = x  ]; then
+    echo "usage: $0 [sw] ifile [sregion [bregion]]"
+    exit 1
+fi
 
+# max size image we will generate when reading a binary table
+MAXDIM=8192
+# block factor to testing image dims
+TBL=8
+
+# for event tables: if binning syntax is not used, calculate a block factor
+echo "$IFILE" | tr '[A-Z]' '[a-z]' |egrep '\[(events|stdevt)\]' 1>/dev/null 2>&1
+if [ $? = 0 ]; then
+  # it's an event file. is binning specified already?
+  echo "$IFILE" | tr '[A-Z]' '[a-z]' | egrep '\]\[bin ' 1>/dev/null 2>&1
+  if [ $? != 0 ]; then
+    # no binning ... make sure we have the appropriate support tools
+    hash fkeyprint 1>/dev/null 2>&1
+    if [ $? = 0 ]; then
+        XEQ=ftools
+    else
+        hash funimage 1>/dev/null 2>&1
+        if [ $? = 0 ]; then
+            XEQ=funtools
+        else
+            XEQ="NONE"
+        fi
+    fi
+    # get image dimensions, if possible
+    case $XEQ in
+    ftools)
+        # first try to bin an event file
+        NAXIS1=`fkeyprint infile=$IFILE'[bin '$TBL']' keynam=NAXIS1 2>/dev/null | egrep = | awk -F= '{print $2}' | awk -F/ '{print $1}' | sed 's/^  *//g;s/  *$//g'`
+        if [ x$NAXIS1 != x ]; then
+            NAXIS2=`fkeyprint infile=$IFILE'[bin '$TBL']' keynam=NAXIS2 2>/dev/null | egrep = | awk -F= '{print $2}' | awk -F/ '{print $1}' | sed 's/^  *//g;s/  *$//g'`
+        fi
+	;;
+    funtools)
+	XIFILE=`echo $IFILE | sed 's#\[.*##'`
+        DIMS=`funimage $XIFILE'[*,*,'$TBL']' stdout bitpix=8 2>/dev/null | dd bs=80 count=10 2>/dev/null | sed -e 's/.\{80\}/&@/g' | tr '@' '\012' | egrep NAXIS'[1,2]' | sed 's# /.*##g;s# ##g'`
+        if [ x"$DIMS" != x ]; then
+            eval $DIMS
+        fi
+        ;;
+    esac
+    # calculate block factor, if possible
+    if [ x"$NAXIS1" != x -a x"$NAXIS2" != x ]; then
+        if [ $NAXIS1 -ge $NAXIS2 ]; then
+            DIM=$NAXIS1
+        else
+            DIM=$NAXIS2
+        fi
+        # calculate a block factor to make the image dimensions reasonable
+        TBL=`echo "(($DIM * $TBL) + ($MAXDIM - 1)) / $MAXDIM" | bc`
+        if [ $TBL -gt 1 ]; then
+            BL="-b $TBL"
+        fi
+    fi
+  fi
+fi
+
+# run regcnts, possibly using a calculated block factor
+regcnts $BL "$@"
